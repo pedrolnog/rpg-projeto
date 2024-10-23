9c57b582a356d056ad5a72fb346ee083706c69f9
Pedro Nogueira <91288432+pedrolnog@users.noreply.github.com>
2024-10-23 15:44:31 -0300

    Add files via upload

---
+++ JogosSalvos.h
@@ -0,0 +1,7 @@
+#ifndef JOGOSSALVOS_H
+#define JOGOSSALVOS_H
+
+struct save;
+void jogosSalvos();
+
+#endif
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "JogosSalvos.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	printf("O Coração de Auryn");
+	printf("1. Jogar\n2. Novo Jogo\n3. Opcoes");
+	jogosSalvos();
+	
+	return 0;
+}
