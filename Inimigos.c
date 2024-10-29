dab23b28c43158e18007b64a9f3bf703f53eed60
Pedro <pedrolucas.nogueira2006@gmail.com>
2024-10-29 17:06:16 -0300

    Adicionadas structs e funções básicas dos inimigos

---
+++ Inimigos.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "inimigos.h"
+# define N 50
+
+struct inimigo_comum{
+	char nome[N];
+	int hp; //Health points
+	int agi; //Agilidade
+	int def; //Defesa
+	int ata; //Ataque
+	int mp; //Mana points
+	char fraq[N]; //Fraqueza
+}typedef ini_comum;
+
+struct inimigo_elite{
+	char nome[N]; 
+	int hp; 
+	int agi; 
+	int def; 
+	int ata;
+	int mp; 
+	char fraq[N];
+	char hab_esp[N]; //Habilidade especial
+}typedef ini_elite;
+
+struct inimigo_boss{
+	char nome[N]; 
+	int hp; 
+	int agi;
+	int def;
+	int ata;
+	int mp; 
+	char hab_esp[N]; 
+	char fraq[N];
+	char imuni[N]; //Imunidade
+	int regen; //Regenera��o
+}typedef ini_boss;
+
+//Fun��o que exibe os status do inimigo comum
+int inimigo_comum(){
+	FILE *inimigoComum; // ponteiro dos status do inimigo comum
+	ini_comum inic; 
+	
+	inimigoComum = fopen("inic_status.txt", "r");
+	if(inimigoComum==NULL){
+		printf("Programa foi de F");
+		return 0;
+	} 
+	//Entrada de Dados via arquivo ".txt"
+	fscanf(inimigoComum, "%s %d %d %d %d %d %s\n",&inic.nome, &inic.hp, &inic.agi, &inic.def,
+	 &inic.ata, &inic.mp, &inic.fraq);
+	 
+	//Exibi��o dos status na Tela
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("                STATUS DO SLIME                \n");
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"); 
+	printf("Nome: %s\nHP: %d\nAgilidade: %d\nDefesa: %d\nAtaque: %d\nMP: %d\nFraqueza: %s\n", inic.nome, inic.hp, inic.agi, 
+	inic.def, inic.ata, inic.mp, inic.fraq);
+	
+	fclose(inimigoComum);
+	system("pause");
+	return 0;
+}
+//Fun��o que exibe os status do inimigo elite
+int inimigo_elite(){
+	FILE *inimigoElite; //Ponteiro dos status do inimigo elite
+	ini_elite inie;
+	
+	inimigoElite = fopen("inie_status.txt", "r");
+	if(inimigoElite == NULL){
+		printf("Programa foi de F");
+		return 0;
+	}
+	
+	//Entrada de Dados via arquivo ".txt"
+	fscanf(inimigoElite, "%s %d %d %d %d %d %s %s\n",&inie.nome, &inie.hp, &inie.agi, &inie.def,
+	 &inie.ata, &inie.mp, &inie.fraq, &inie.hab_esp);
+	 
+	//Exibi��o dos status na Tela 
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("          STATUS DO CAVALEIRO NEGRO            \n");
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("Nome: %s\nHP: %d\nAgilidade: %d\nDefesa: %d\nAtaque: %d\nMP: %d\nFraqueza: %s\nHabilidade especial: %s\n", 
+	inie.nome, inie.hp, inie.agi, inie.def, inie.ata, inie.mp, inie.fraq, inie.hab_esp);
+	
+	fclose(inimigoElite);
+	system("pause");
+	return 0;
+}
+
+//Fun��o que exibe os status do inimigo Boss
+
+int inimigo_boss(){
+	FILE *inimigoBoss;
+	ini_boss inib;
+	
+	inimigoBoss = fopen("inib_status.txt", "r");
+	if(inimigoBoss == NULL){
+		printf("O programa foi de F");
+		return 0;
+	}
+	
+	//Entrada de Dados via arquivo ".txt"
+	fscanf(inimigoBoss, "%s %d %d %d %d %d %s %s %d\n",&inib.nome, &inib.hp, &inib.agi, &inib.def,
+	 &inib.ata, &inib.mp, &inib.fraq, &inib.hab_esp, &inib.regen);
+	 
+	//Exibi��o dos status na Tela 
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("            STATUS DO BOSS                     \n");
+	printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("Nome: %s\nHP: %d\nAgilidade: %d\nDefesa: %d\nAtaque: %d\nMP: %d\nFraqueza: %s\nHabilidade especial: %s\nRegenera��o %d%%\n", 
+	inib.nome, inib.hp, inib.agi, inib.def, inib.ata, inib.mp, inib.fraq, inib.hab_esp, inib.regen);
+	
+	
+	fclose(inimigoBoss);
+	system("pause");
+	return 0;
+	
+}
+
+
+
+
+
+
+
---
+++ Inimigos.h
@@ -0,0 +1,7 @@
+#ifndef INIMIGOS_H
+#define INIMIGOS_H
+
+int inimigo_comum();
+int inimigo_elite();
+int inimigo_boss();
+#endif
