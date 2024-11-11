760375b02b79da8d6746de974ce7330ad65d0d11
Dalário Aerton <aertondalario@gmail.com>
2024-11-11 10:33:00 -0300

    Update Inimigos.h

--- Inimigos.h
+++ Inimigos.h
@@ -1,7 +1,6 @@
 #ifndef INIMIGOS_H
 #define INIMIGOS_H
-
-
+#define N 50
 struct inimigo_comum{
 	char nome[N];
 	int hp; //Health points
@@ -33,11 +32,28 @@ struct inimigo_boss{
 	char hab_esp[N]; 
 	char fraq[N];
 	char imuni[N]; //Imunidade
-	int regen; //Regenera��o
+	int regen; //Regeneração
 }typedef ini_boss;
 
-int inimigo_comum();
-int inimigo_elite();
-int inimigo_boss();
+FILE *ic; //Ponteiro dos status do inimigo comum
+FILE *ie; //Ponteiro dos status do inimigo elite
+FILE *ib; //Ponteiro dos status do Boss
+
+ini_comum inic;
+ini_elite inie;
+ini_boss inib;
+
+int ler_inimigo_comum(); //Apresenta os status do inimigo comum
+int ler_inimigo_elite(); //Apresenta os status do inimigo elite
+int ler_inimigo_boss(); //Apresenta os status do Boss
+int apa_inimigo_comum(); //Apaga os dados salvos na struct de inimigo comum
+int apa_inimigo_elite(); //Apaga os dados salvos na struc de inimigo elite
+int apa_inimigo_boss(); //Apaga os dados salvos na struct de inimigo boss	
+int upd_inimigo_comum(); //Escreve os status no arquivo .txt de inimigos comum
+int upd_inimigo_elite(); //Escreve os status no arquivo .txt de inimigos elite
+int upd_inimigo_boss(); //Escreve os status no arquivo .txt de inimigos boss
+int cri_inimigo_comum(); //Cria um aqruivo .txt com os status do inimigo comum
+int cri_inimigo_elite(); //Cria um arquivo .txt com os status do inimigo elite
+int cri_inimigo_boss(); //Cria um um arquivo .txt com os status do inimigo boss
 
 #endif
