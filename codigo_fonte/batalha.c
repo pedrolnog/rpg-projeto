5fb0b1dbd332f9650b9c13dadfd21dd482797dfa
Dalário Aerton <aertondalario@gmail.com>
2024-11-18 04:19:10 -0300

    Funções de batalha funcionais

--- codigo_fonte/batalha.c
+++ codigo_fonte/batalha.c
@@ -1,66 +1,340 @@
+#include <Windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "inimigos.h"
+#include "savegame.h"
+#include "menuBatalha.h"
 
-int ataque(/*se tiver algum buuff ele vai vim como parametro*/){
-	// srand(time(NULL));
-
+int ataquenoSlime(/*se tiver algum buuff ele vai vim como parametro*/){
 	int ataque;
+	srand(time(NULL));
+	
+	ataque = rand() % 20; 
+	
+	if(ataque < 3){ /* um erro critico nï¿½o pode ser afetado por buffs*/
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        VOCE TEVE UM ERRO CRITICO...             \n");
+	printf("        E deu 0 de dano                            ");
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		upd_inimigo_comum();
+		return inimigoComum.hp;
+	}
+	
+	else if(ataque/*+possivel buff*/ <= 5 && ataque >= 3/*agilidade do inimigo(nesse caso slime)*/){
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        O SLIME DO VAZIO DESVIOU...              \n");
+	printf("        E perdeu 0 pontos de vida                  ");
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		upd_inimigo_comum();
+		return inimigoComum.hp;
+	}
+	else if(ataque <= 6/*defesa do inimigo(nesse caso slime)*/){
+	//	danoDoAtaque = rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque += rand() % (6 - 1 + 1) + 1;
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        O SLIME DO  VAZIO DEFENDEU!!!            \n");
+	printf("         tirou %d pontos de vida do slime      ", ataque);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	if((inimigoComum.hp - ataque ) <= 0){
+	printf("       O SLIME DO VAZIO FOI NOCAUTEADO!!           ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoComum.hp = inimigoComum.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		
+		upd_inimigo_comum();
+		return inimigoComum.hp;
+		
+	}
+	
+	else if(ataque > 6 && ataque < 10){
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        VOCE ACERTOU!!!                          \n");
+	printf("        E tirou %d pontos de vida do slime      ", ataque);
+	printf("\n-----------------------------------------------\n");
+	if((inimigoComum.hp - ataque ) <= 0){
+	printf("       O SLIME DO VAZIO FOI NOCAUTEADO!!           ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoComum.hp = inimigoComum.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		
+		upd_inimigo_comum();
+		return inimigoComum.hp;
+	}
+	
+	else if(ataque/*+possivel buff*/ >= 10){
+	//	danoDoAtaque = rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque += rand() % (6 - 1 + 1) + 1;
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        VOCE ACERTOU!!!                          \n");
+	printf("        E tirou %d pontos de vida do slime      ", ataque);
+	printf("\n-----------------------------------------------\n");
+	if((inimigoComum.hp - ataque ) <= 0){
+	printf("       O SLIME DO VAZIO FOI NOCAUTEADO!!           ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoComum.hp = inimigoComum.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		
+		upd_inimigo_comum();
+		return inimigoComum.hp;
+	}
+	else if(ataque == 20){ /*um acerto critico nï¿½o pode depender de buffs*/
+	//	danoDoAtaque = rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque += rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque *= 2; 
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=\n");
+	printf("        VOCE ACERTOU UM ATAQUE CRITICO                 \n");
+	printf("        E tirou %d pontos de vida do slime do vazio      ", 2 * ataque);
+	printf("\n-----------------------------------------------------\n");
+	if((inimigoComum.hp - ataque ) <= 0){
+	printf("       O SLIME DO VAZIO FOI NOCAUTEADO!!           ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoComum.hp = inimigoComum.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		upd_inimigo_comum();
+		return inimigoComum.hp;
+	}
+}
+
+int ataqueSlime(){
+    int ataque;
 	int danoDoAtaque;
+	srand(time(NULL));
 	
-	ataque = rand() % (20 - 1 + 1) + 1; //gera um numero aleatorio entre 1 e 20 que decidir� se o golpe ira acertar o inimigo ou n�o
+	ataque = rand() % 10;
 	
-	if(ataque == 1){ /* um erro critico n�o pode ser afetado por buffs*/
-		printf("Voce errou o ataque. \n");
-		return 0;
+	if(ataque < 1)/* um erro critico nï¿½o pode ser afetado por buffs*/{ 
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        O SLIME DO VAZIO TE ATACA...             \n");
+	printf("        Ele errou o golpe                          ");
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	return heroi.hp;
 	}
-	else if(ataque/*+possivel buff*/ <= 10/*agilidade do inimigo(nesse caso slime)*/){
-		printf("o Slime desviou do ataque.\n");
-		return 0;
+	
+	else{
+		ataque = ataque;
+		heroi.hp = heroi.hp - ataque;
+		sleep(2);
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		printf("        O SLIME DO VAZIO TE ATACA...             \n");
+		printf("        Ele te tirou %d pontos de vida             ",ataque);
+		printf("\n-----------------------------------------------\n");
+		printf("        Voce ainda tem %d pontos de vida           ",heroi.hp);
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		return heroi.hp;
 	}
-	else if(ataque <= 10/*defesa do inimigo(nesse caso slime)*/){
-		danoDoAtaque = rand() % (6 - 1 + 1) + 1;
-		danoDoAtaque += rand() % (6 - 1 + 1) + 1;
-		danoDoAtaque = danoDoAtaque/2;
-		printf("O Slime defendeu o seu ataque e portanto so recebeu %d de dano!\n");
-		return danoDoAtaque;
-	}
-	else if(ataque/*+possivel buff*/ > 10){
-		danoDoAtaque = rand() % (6 - 1 + 1) + 1;
-		danoDoAtaque += rand() % (6 - 1 + 1) + 1;
-		printf("Voce acertou e causou %d de dano no Slime!\n", danoDoAtaque);
-		return danoDoAtaque;
-	}
-	else if(ataque == 20){ /*um acerto critico n�o pode depender de buffs*/
-		danoDoAtaque = rand() % (6 - 1 + 1) + 1;
-		danoDoAtaque += rand() % (6 - 1 + 1) + 1;
-		danoDoAtaque *= 2; 
-		printf("Voce acertou um golpe critico e causou %d de dano no Slime!\n", danoDoAtaque);
-		return danoDoAtaque;
-	}
+
 }
 
 int bloquear(int danoDoSlime/*+bonus de buffs*/){ /*garante uma defesa*/
-	return danoDoSlime/2 /*+buff, para reduzir ainda mais o dano*/;
+	int ataque;
+	
+	ataque = rand() % 5;
+	
+	ataque = ataque;
+	heroi.hp = heroi.hp - ataque;
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("       VOCE DEFENDEU!!!                          \n");
+	printf("       E conseguiu diminuir o dano para %d         ", ataque);
+	printf("\n-----------------------------------------------\n");
+	printf("        Voce ainda tem %d pontos de vida\n         ",heroi.hp);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	return heroi.hp;
 }
 
-int fugir(int agilidadeHeroi, int agilidadeInimigo){ //no caso do tutorial n�o vai ter como escapar ent�o da pra so colocar uma mensagem dizendo que algo prende ele no combate.
-	if(agilidadeHeroi/*+buff*/ > agilidadeInimigo){
-		printf("Voce escapou com sucesso!\n");
-		return 0;
-	}
-	else{
-		printf("Voce nao consegue escapar!\n");
+int fugir(int agilidadeHeroi, int agilidadeInimigo){ //no caso do tutorial nï¿½o vai ter como escapar entï¿½o da pra so colocar uma mensagem dino combate.
+	sleep(2);
+	printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+	printf("           Voce nao consegue escapar!            ");
+	printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
 		return 1;
 	}
-}
 
 int usar_item(){
 	int escolhaItem;
+	int quantidadePocao = 999; //infinitas
 	do{
-	printf("MOCHILA:\n1.Pocao de Cura(3x)\n5.Sair\n");
+	sleep(2);
+	printf("MOCHILA:\n1.Pocao de Cura(%dx)\n5.Sair\n", quantidadePocao);
 	scanf("%d", &escolhaItem);
+	if(escolhaItem == 1){
+		heroi.hp = heroi.hp + 10;
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		printf("                +10 HP RESTAURADO                 ");
+		printf("\n-----------------------------------------------\n");
+		printf("            Voce tem %d pontos de vida\n           ",heroi.hp);
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	}
 	} while(escolhaItem != 1 && escolhaItem != 5);
 
 	return escolhaItem;
+}
+
+
+int ataquenoCavaleiro(/*se tiver algum buuff ele vai vim como parametro*/){
+	int ataque;
+	int danoDoAtaque;
+	srand(time(NULL));
+	
+	ataque = rand() % 20; 
+	
+	if(ataque < 3){ /* um erro critico nï¿½o pode ser afetado por buffs*/
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        VOCE TEVE UM ERRO CRITICO...             \n");
+	printf("        E deu 0 de dano                            ");
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		upd_inimigo_elite();
+		return inimigoElite.hp;
+	}
+	
+	else if(ataque/*+possivel buff*/ <= 5 && ataque >= 3/*agilidade do inimigo(nesse caso o Cavaleiro)*/){
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("       O CAVALEIRO NEGRO DESVIOU                 \n");
+	printf("       E perdeu 0 pontos de vida                  ");
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		upd_inimigo_elite();
+		return inimigoElite.hp;
+	}
+	else if(ataque <= 6/*defesa do inimigo(nesse caso cavaleiro)*/){
+	//	danoDoAtaque = rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque += rand() % (6 - 1 + 1) + 1;
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        O CAVALEIRO NEGRO DEFENDEU!!!            \n");
+	printf("        Mas perdeu %d pontos de vida               ", ataque);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	if((inimigoElite.hp - ataque ) <= 0){
+	printf("       O CAVALEIRO NEGRO FOI NOCAUTEADO!!           ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoElite.hp = inimigoElite.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	
+		
+		upd_inimigo_elite();
+		return inimigoComum.hp;
+		
+	}
+	
+	else if(ataque > 6 && ataque < 10){
+	danoDoAtaque = ataque;
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        VOCE ACERTOU!!!                          \n");
+	printf("        E tirou %d pontos de vida do cavaleiro     ", ataque);
+	printf("\n-----------------------------------------------\n");
+	if((inimigoElite.hp - ataque ) <= 0){
+	printf("       O CAVALEIRO NEGRO FOI NOCAUTEADO!!          ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoElite.hp = inimigoElite.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		
+		upd_inimigo_elite();
+		return inimigoElite.hp;
+	}
+	
+	else if(ataque/*+possivel buff*/ >= 10){
+	//	danoDoAtaque = rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque += rand() % (6 - 1 + 1) + 1;
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        VOCE ACERTOU!!!                          \n");
+	printf("        E tirou %d pontos de vida do cavaleiro     ", ataque);
+	printf("\n-----------------------------------------------\n");
+	if((inimigoElite.hp - ataque ) <= 0){
+	printf("       O CAVALEIRO NEGRO FOI NOCAUTEADO!!          ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoElite.hp = inimigoElite.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		
+		upd_inimigo_elite();
+		return inimigoElite.hp;
+	}
+	else if(ataque == 20){ /*um acerto critico nï¿½o pode depender de buffs*/
+	//	danoDoAtaque = rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque += rand() % (6 - 1 + 1) + 1;
+	//	danoDoAtaque *= 2; 
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=\n");
+	printf("        VOCE ACERTOU UM ATAQUE CRITICO                 \n");
+	printf("        E tirou %d pontos de vida do cavaleiro negro     ", 2 * ataque);
+	printf("\n-----------------------------------------------------\n");
+	if((inimigoElite.hp - ataque ) <= 0){
+	printf("       O CAVALEIRO NEGRO FOI NOCAUTEADO!!          ");
+	return 0;
+	}
+	else{
+	printf("        Ele ainda tem %d pontos de vida            ", inimigoElite.hp = inimigoElite.hp - ataque);
+	}
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		upd_inimigo_elite();
+		return inimigoElite.hp;
+	}
+}
+
+int ataqueCavaleiro(){
+    int ataque;
+	int danoDoAtaque;
+	srand(time(NULL));
+	
+	ataque = rand() % 15;
+	
+	if(ataque < 1)/* um erro critico nï¿½o pode ser afetado por buffs*/{ 
+	sleep(2);
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	printf("        O CAVALEIRO NEGRO TE ATACA...            \n");
+	printf("        Ele errou o golpe                          ");
+	printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	return heroi.hp;
+	}
+	else if(ataque == 15){
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		printf("        O CAVALEIRO NEGRO TE ATACA...            \n");
+		printf("        ELE USA O CORTE FANTASMA!!!!             \n");
+		printf("\n-----------------------------------------------\n");
+		printf("        Voce ainda tem %d pontos de vida           ",heroi.hp);
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+	}
+	else{
+		ataque = ataque;
+		heroi.hp = heroi.hp - ataque;
+		sleep(2);
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		printf("        O CAVALEIRO NEGRO TE ATACA...            \n");
+		printf("        Ele te tirou %d pontos de vida             ",ataque);
+		printf("\n-----------------------------------------------\n");
+		printf("        Voce ainda tem %d pontos de vida           ",heroi.hp);
+		printf("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+		return heroi.hp;
+	}
+
 }
